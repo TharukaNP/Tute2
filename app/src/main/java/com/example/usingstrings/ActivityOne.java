17fa203421bf447098b20a8ba39954bc21d85d7d
Tharuka Nadeesh <tharukanadeesh@icloud.com>
2020-08-09 00:14:17 +0530

    initial commit

---
+++ app/src/main/java/com/example/usingstrings/ActivityOne.java
@@ -0,0 +1,66 @@
+package com.example.usingstrings;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class ActivityOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView txtVMsg2 = findViewById(R.id.TvMsg2);
+        txtVMsg2.setText(R.string.Msg2);
+
+        Log.i("Lifecycle", "OnCreate called...");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.i("Lifecycle", "OnStart called...");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        Log.i("Lifecycle", "OnRestart called...");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.i("Lifecycle", "OnResume called...");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.i("Lifecycle", "OnPause called...");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.i("Lifecycle", "OnStop called...");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.i("Lifecycle", "OnDestroy called...");
+    }
+}
